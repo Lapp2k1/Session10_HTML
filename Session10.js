9f1675527fef75e3bd1e71b2f8a1213d05aa4058
Lapp2k1 <vulengocvip2015@gmail.com>
2024-04-23 17:07:23 +0700

    first commit

---
+++ Session10.js
@@ -0,0 +1,15 @@
+ function myFunction() { document.getElementById("demo").innerHTML = "Paragraph changed."
+ }
+// let a="nice";
+// let b=4;
+// let c;
+// console.log(c);
+
+// let a=1
+// a=+prompt("Nhập a");
+// let b=2
+// b=+prompt("Nhập b");
+// console.log(a+b);
+let d = "deptrai";
+
+console.log(d.toUpperCase());
